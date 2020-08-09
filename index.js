1c3426f4e8e197627f75797a019511bda87a4cb3
nitinkumar98 <nitinkumar0410@gmail.com>
2020-08-09 15:52:14 +0530

    first commit

--- index.js
+++ index.js
@@ -1,21 +1,22 @@
 // node server which will handle socket.io connection
-const io = require("socket.io")(8000);
+const io = require("socket.io")(process.env.PORT || 8000);
 
 const users = {};
 
 io.on("connection", (socket) => {
+  // If any new joins, let other users connected to the server know!
   socket.on("new-user-joined", (name) => {
     users[socket.id] = name;
     socket.broadcast.emit("user-joined", name);
   });
-
+  // If someone sends a message, broadcast it to the other people
   socket.on("send", (message) => {
     socket.broadcast.emit("receive", {
       message: message,
       name: users[socket.id],
     });
   });
-
+  // If someone leaves the chat .let other know!
   socket.on("disconnect", (message) => {
     socket.broadcast.emit("left", users[socket.id]);
     delete users[socket.id];
